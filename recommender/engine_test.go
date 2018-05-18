c65c22a11ecd9dd265e65ec5693f423b870d9972
Laszlo Puskas <laszlo@banzaicloud.com>
2018-05-18 16:53:31 +0200

    wip - cleaned the recommendation implementation, organized, structured the code

--- recommender/engine_test.go
+++ recommender/engine_test.go
@@ -6,12 +6,13 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+type dummyVmRegistry struct {
+	// implement the interface
+	VmRegistry
+}
+
 func TestNewEngine(t *testing.T) {
 
-	type dummyVmRegistry struct {
-		// implement the interface
-		VmRegistry
-	}
 	tests := []struct {
 		name         string
 		vmRegistries map[string]VmRegistry
@@ -29,7 +30,7 @@ func TestNewEngine(t *testing.T) {
 			name:         "engine creation fails when registries is nil",
 			vmRegistries: nil,
 			checker: func(engine *Engine, err error) {
-				assert.Nil(t, engine, "the e ngine should be nil")
+				assert.Nil(t, engine, "the engine should be nil")
 				assert.NotNil(t, err, "the error shouldn't be nil")
 			},
 		},
@@ -37,6 +38,39 @@ func TestNewEngine(t *testing.T) {
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
 			test.checker(NewEngine(test.vmRegistries))
+
+		})
+	}
+}
+
+func TestEngine_RecommendCluster(t *testing.T) {
+	tests := []struct {
+		name         string
+		vmRegistries map[string]VmRegistry
+		request      ClusterRecommendationReq
+		provider     string
+		region       string
+		check        func(response *ClusterRecommendationResp, err error)
+	}{
+		{
+			name:         "cluster recommendation success",
+			vmRegistries: map[string]VmRegistry{"ec2": dummyVmRegistry{}},
+			request:      ClusterRecommendationReq{},
+			provider:     "ec2",
+			region:       "us",
+
+			check: func(response *ClusterRecommendationResp, err error) {
+				assert.Nil(t, err, "should not get error when recommending")
+
+			},
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			engine, err := NewEngine(test.vmRegistries)
+			assert.Nil(t, err, "the engine couldn't be created")
+
+			test.check(engine.RecommendCluster(test.provider, test.region, test.request))
 
 		})
 	}
