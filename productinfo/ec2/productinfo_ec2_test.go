683137e6131efd212c520e63f8e250d976d05b9f
Attila Prekopcsák <attila.prekopcsak@banzaicloud.com>
2018-05-26 12:42:51 +0200

    unit tests for productinfo_ec2 operations

--- productinfo/ec2/productinfo_ec2_test.go
+++ productinfo/ec2/productinfo_ec2_test.go
@@ -1,9 +1,11 @@
 package ec2
 
 import (
-	"fmt"
 	"testing"
 
+	"errors"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/endpoints"
 	"github.com/aws/aws-sdk-go/service/pricing"
 	"github.com/banzaicloud/cluster-recommender/productinfo"
 	"github.com/stretchr/testify/assert"
@@ -56,13 +58,21 @@ func (dps *DummyPricingSource) GetAttributeValues(input *pricing.GetAttributeVal
 			},
 		}, nil
 	case 3:
-		return nil, fmt.Errorf("failed to retrieve values")
+		return nil, errors.New("failed to retrieve values")
 	}
 
 	return nil, nil
 }
 func (dps *DummyPricingSource) GetProducts(input *pricing.GetProductsInput) (*pricing.GetProductsOutput, error) {
+	switch dps.TcId {
+	case 4:
+		return &pricing.GetProductsOutput{
+			PriceList: []aws.JSONValue{},
+		}, nil
+	case 5:
+		return nil, errors.New("failed to retrieve values")
 
+	}
 	return nil, nil
 }
 
@@ -112,6 +122,144 @@ func TestEc2Infoer_GetAttributeValues(t *testing.T) {
 
 			test.check(productInfoer.GetAttributeValues(test.attrName))
 
+		})
+	}
+}
+
+func TestEc2Infoer_GetRegions(t *testing.T) {
+	tests := []struct {
+		name          string
+		pricingServie PricingSource
+		check         func(regionId map[string]string)
+	}{
+		{
+			name:          "receive all regions",
+			pricingServie: &DummyPricingSource{},
+			check: func(regionId map[string]string) {
+				assert.NotNil(t, regionId, "the regionId shouldn't be nil")
+			},
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			productInfoer, err := NewEc2Infoer(test.pricingServie)
+			if err != nil {
+				t.Fatalf("failed to create productinfoer; [%s]", err.Error())
+			}
+
+			test.check(productInfoer.GetRegions())
+		})
+	}
+}
+
+func TestEc2Infoer_GetProducts(t *testing.T) {
+	tests := []struct {
+		name          string
+		regionId      string
+		attrKey       string
+		attrValue     productinfo.AttrValue
+		pricingServie PricingSource
+		check         func(vm []productinfo.Ec2Vm, err error)
+	}{
+		{
+			name:          "successful",
+			regionId:      "eu-central-1",
+			attrKey:       productinfo.Cpu,
+			attrValue:     productinfo.AttrValue{Value: float64(2), StrValue: productinfo.Cpu},
+			pricingServie: &DummyPricingSource{TcId: 4},
+			check: func(vm []productinfo.Ec2Vm, err error) {
+				assert.Nil(t, err, "the error should be nil")
+				assert.Nil(t, vm, "the vm should be nil")
+			},
+		},
+		{
+			name:          "error - GetProducts",
+			regionId:      "eu-central-1",
+			attrKey:       productinfo.Cpu,
+			attrValue:     productinfo.AttrValue{Value: float64(2), StrValue: productinfo.Cpu},
+			pricingServie: &DummyPricingSource{TcId: 5},
+			check: func(vm []productinfo.Ec2Vm, err error) {
+				assert.Equal(t, err, errors.New("failed to retrieve values"))
+				assert.Nil(t, vm, "the vm should be nil")
+			},
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			productInfoer, err := NewEc2Infoer(test.pricingServie)
+			if err != nil {
+				t.Fatalf("failed to create productinfoer; [%s]", err.Error())
+			}
+
+			test.check(productInfoer.GetProducts(test.regionId, test.attrKey, test.attrValue))
+		})
+	}
+}
+
+func TestEc2Infoer_GetRegion(t *testing.T) {
+	tests := []struct {
+		name          string
+		id            string
+		pricingServie PricingSource
+		check         func(region *endpoints.Region)
+	}{
+		{
+			name:          "unknown region",
+			id:            "testRegion",
+			pricingServie: &DummyPricingSource{},
+			check: func(region *endpoints.Region) {
+				assert.Nil(t, region, "the region should be nil")
+			},
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			productInfoer, err := NewEc2Infoer(test.pricingServie)
+			if err != nil {
+				t.Fatalf("failed to create productinfoer; [%s]", err.Error())
+			}
+
+			test.check(productInfoer.GetRegion(test.id))
+		})
+	}
+}
+
+func TestNewConfig(t *testing.T) {
+	tests := []struct {
+		name  string
+		check func(config *aws.Config)
+	}{
+		{
+			name: "success - create a new config instance",
+			check: func(config *aws.Config) {
+				assert.NotNil(t, config, "the config should be nil")
+			},
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			test.check(NewConfig())
+		})
+	}
+}
+
+func TestNewPricing(t *testing.T) {
+	tests := []struct {
+		name  string
+		cfg   *aws.Config
+		check func(source PricingSource)
+	}{
+		{
+			name: "success - create a new PricingSource",
+			cfg:  NewConfig(),
+			check: func(source PricingSource) {
+				assert.NotNil(t, source, "the source should be nil")
+			},
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			test.check(NewPricing(test.cfg))
 		})
 	}
 }
