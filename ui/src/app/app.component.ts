6e5155b1268ef6b0bd8d20817dfdda39eb411108
Marton Sereg <martonsereg@gmail.com>
2018-06-15 15:58:03 +0200

    added a static material table

--- ui/src/app/app.component.ts
+++ ui/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Banzai Cloud Telescopes';
+  columnsToDisplay = ['machineType', 'cpu', 'mem'];
+  myDataArray = [
+    {"type": "m5.large", "cpus": 4, "memory": 8},
+    {"type": "m5.xlarge", "cpus": 8, "memory": 16},
+  ];
 }
