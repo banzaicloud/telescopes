ebf5075f9be5107bc14d6fbeeb55a14068077a3b
Attila Prekopcsák <attila.prekopcsak@banzaicloud.com>
2018-06-22 11:35:48 +0200

    gce network perf

--- productinfo/gce/network_mapper.go
+++ productinfo/gce/network_mapper.go
@@ -1,15 +1,16 @@
 package gce
 
 import (
+	"fmt"
 	"github.com/banzaicloud/telescopes/productinfo"
 )
 
 var (
-	// TODO
 	ntwPerfMap = map[string][]string{
-		productinfo.NTW_LOW:    {"Low"},
-		productinfo.NTW_MEDIUM: {"Moderate"},
-		productinfo.NTW_HIGH:   {""},
+		productinfo.NTW_LOW:    {"1", "2"},
+		productinfo.NTW_MEDIUM: {"4", "6", "8"},
+		productinfo.NTW_HIGH:   {"10", "12", "14"},
+		productinfo.NTW_EXTRA:  {"16"},
 	}
 )
 
@@ -24,5 +25,19 @@ func newGceNetworkMapper() *GceNetworkMapper {
 
 // MapNetworkPerf maps the network performance of the gce instance to the category supported by telescopes
 func (nm *GceNetworkMapper) MapNetworkPerf(vm productinfo.VmInfo) (string, error) {
-	return ntwPerfMap[productinfo.NTW_MEDIUM][0], nil
+	for perfCat, strVals := range ntwPerfMap {
+		if contains(strVals, vm.NtwPerf) {
+			return perfCat, nil
+		}
+	}
+	return "", fmt.Errorf("could not determine network performance for: [%s]", vm.NtwPerf)
+}
+
+func contains(slice []string, val string) bool {
+	for _, v := range slice {
+		if v == val {
+			return true
+		}
+	}
+	return false
 }
