47695fc456af5e3c6de2e4db70bee089ddfe1a3f
Attila Prekopcsák <attila.prekopcsak@banzaicloud.com>
2019-01-24 16:16:34 +0100

    add test

--- pkg/recommender/engine_test.go
+++ pkg/recommender/engine_test.go
@@ -532,3 +532,46 @@ func TestEngine_RecommendCluster(t *testing.T) {
 		})
 	}
 }
+
+func TestEngine_avgSpotNodeCount(t *testing.T) {
+	tests := []struct {
+		name     string
+		minNodes int
+		maxNodes int
+		odNodes  int
+		check    func(resp int)
+	}{
+		{
+			name:     "0 < on demand percentage < 100",
+			minNodes: 1,
+			maxNodes: 2,
+			odNodes:  1,
+			check: func(resp int) {
+				assert.Equal(t, 1, resp)
+			},
+		},
+		{
+			name:     "on demand percentage = 100",
+			minNodes: 1,
+			maxNodes: 2,
+			odNodes:  2,
+			check: func(resp int) {
+				assert.Equal(t, 0, resp)
+			},
+		},
+		{
+			name:     "on demand percentage = 0",
+			minNodes: 1,
+			maxNodes: 2,
+			odNodes:  0,
+			check: func(resp int) {
+				assert.Equal(t, 2, resp)
+			},
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			test.check(avgSpotNodeCount(test.minNodes, test.maxNodes, test.odNodes))
+		})
+	}
+}
